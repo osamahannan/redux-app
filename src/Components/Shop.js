22feb370f92cf10bfdea53a87e0fc22b4b9523d0
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-11-13 14:10:05 +0530

    installed redux, react-redux, redux-thunk for our app and updated home page

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -3,8 +3,9 @@ import React from 'react'
 const Shop = () => {
     return (
         <div className="container my-4">
+            <h2 className="my-4">Deposite/Withdraw Money</h2>
             <button className="btn btn-primary mx-2">-</button>
-            Add this item to cart
+            Update Balance
             <button className="btn btn-primary mx-2">+</button>
         </div> 
     )
