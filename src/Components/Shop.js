d4108080a2b46af7b7597e77551ed09da5436789
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-11-13 13:55:02 +0530

    added navbar for redux app

---
+++ src/Components/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div className="container my-4">
+            <button className="btn btn-primary mx-2">-</button>
+            Add this item to cart
+            <button className="btn btn-primary mx-2">+</button>
+        </div> 
+    )
+}
+
+export default Shop
