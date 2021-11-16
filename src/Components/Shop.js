f1b5516c0095634bcd5b5c19c1950f14a86a1517
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-11-16 18:39:15 +0530

    added dispatch function to the shop.js

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { actionCreators } from '../State/index'
 
 const Shop = () => {
+
+    const dispatch = useDispatch();
+
     return (
         <div className="container my-4">
             <h2 className="my-4">Deposite/Withdraw Money</h2>
-            <button className="btn btn-primary mx-2">-</button>
+            <button className="btn btn-primary mx-2" onClick={() => dispatch(actionCreators.withdrawMoney(100))}>-</button>
             Update Balance
-            <button className="btn btn-primary mx-2">+</button>
+            <button className="btn btn-primary mx-2" onClick={() => dispatch(actionCreators.depositMoney(100))}>+</button>
         </div> 
     )
 }
