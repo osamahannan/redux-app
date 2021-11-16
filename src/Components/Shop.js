1a1639700d31b72b7e859113fad832d2e0ee93f6
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-11-16 18:48:28 +0530

    applied destructuring in shop.js

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -6,14 +6,14 @@ import { actionCreators } from '../State/index'
 const Shop = () => {
 
     const dispatch = useDispatch();
-    const actions = bindActionCreators(actionCreators, dispatch)
+    const {withdrawMoney, depositMoney} = bindActionCreators(actionCreators, dispatch)
 
     return (
         <div className="container my-4">
             <h2 className="my-4">Deposite/Withdraw Money</h2>
-            <button className="btn btn-primary mx-2" onClick={() => actions.withdrawMoney(100)}>-</button>
+            <button className="btn btn-primary mx-2" onClick={() => withdrawMoney(100)}>-</button>
             Update Balance
-            <button className="btn btn-primary mx-2" onClick={() => actions.depositMoney(100)}>+</button>
+            <button className="btn btn-primary mx-2" onClick={() => depositMoney(100)}>+</button>
         </div> 
     )
 }
